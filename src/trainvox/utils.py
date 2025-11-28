a40295ff1efde7b04dd9518abd7e6840ead9ca5d
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2025-11-28 14:10:44 +0100

    feat(utils): add send_telegram_photo function
    Add a utility to send local photos with optional captions to Telegram chats
    using the bot API. Update send_telegram_message to use MarkdownV2 and improve
    type annotations and docstrings. Raise for HTTP errors in both functions.

--- src/trainvox/utils.py
+++ src/trainvox/utils.py
@@ -1,21 +1,58 @@
+from pathlib import Path
+
 import requests
 
 
-def send_telegram_message(msg: str, token: str, chat_id: str) -> None:
+def send_telegram_message(msg: str, token: str, chat_id: int | str) -> None:
     """Send a message on Telegram.
 
-    The message can be formatted using Markdown.
+    Args:
+        msg: The message to send. Can be formatted using MarkdownV2
+        token: The token of the Telegram bot
+        chat_id: The unique identifier for the target chat
+
     """
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+
     payload = {
         "chat_id": chat_id,
-        "parse_mode": "Markdown",
+        "parse_mode": "MarkdownV2",
         "disable_web_page_preview": True,
         "text": msg,
     }
 
-    r = requests.get(
-        f"https://api.telegram.org/bot{token}/sendMessage",
-        params=payload,
-        timeout=10,
-    )
+    r = requests.get(url, params=payload, timeout=20)
+    r.raise_for_status()
+
+
+def send_telegram_photo(
+    photo_path: str | Path,
+    caption: str | None,
+    token: str,
+    chat_id: int | str,
+) -> None:
+    """Send a local photo on Telegram.
+
+    Args:
+        photo_path: The path to an image file on disk
+        caption: The caption of the photo. Can be formatted using MarkdownV2
+        token: The token of the Telegram bot
+        chat_id: The unique identifier for the target chat
+
+    """
+    url = f"https://api.telegram.org/bot{token}/sendPhoto"
+
+    photo_path = Path(photo_path)
+
+    payload = {
+        "chat_id": chat_id,
+        "parse_mode": "MarkdownV2",
+    }
+    if caption:
+        payload["caption"] = caption
+
+    with photo_path.open("rb") as img:
+        files = {"photo": img}
+        r = requests.post(url, data=payload, files=files, timeout=20)
+
     r.raise_for_status()
